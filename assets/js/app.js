a652b0db3dd10944c801a077df68750d0668c8f6
Blanca <blanca@MacBook-de-Blanca.local>
2022-10-01 16:03:22 +0000

    prompt y ajustes menores de bootstrap

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,3 +11,7 @@ $(document).scroll(function () {
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
   
+var enviar = $("#enviar");
+enviar.click(function (){
+    alert("El mensaje fue enviado correctamente");
+});
